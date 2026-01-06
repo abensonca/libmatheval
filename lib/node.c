8c532cd0a0106037ff6bb5ced1348e8059d38c2e
Andrew Benson <abenson@obs.carnegiescience.edu>
2026-01-06 10:22:58 -0800

    more debug

--- lib/node.c
+++ lib/node.c
@@ -1558,6 +1558,7 @@ node_get_length(Node * node)
 
 	/* According to node type, calculate length of string representing 
 	 * subtree rooted at node. */
+	printf("START\n");
 	  printf("TYPE %s\n",node->type);
 	switch (node->type) {
 	case 'n':
